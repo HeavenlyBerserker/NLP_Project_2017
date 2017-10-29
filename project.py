08ea2cbf82f6a850135426f2cafadcc46e3bcb17
HeavenlyBerserker <hxu.yunocreative@gmail.com>
2017-10-29 12:40:13 -0600

    Changes in structure of project.py Added some functions to clear main.

--- project.py
+++ project.py
@@ -1,52 +1,64 @@
+###########################
+#NLP Project 2017 main code
+###########################
 
+#Autors: Hong Xu and Jie Zhang
+#Repository: https://github.com/HeavenlyBerserker/NLP_Project_2017.git
 
-
+#Possibly useful imports
+import argparse
+import sys
+import getopt
+import math
+import re
+import random
 import os
+#NLP tools
 import nltk
 
-def main():
-    filedir='developset/texts'
-    for filename in os.listdir(filedir):
-        print (filename)
-        print ("###############################################################")
-        
-        answer=find(filename, 'developset/answers')
-        with open(answer+".anskey", 'r') as input_file:
-            prevrole=""
-            for line in input_file:
-                if ":" in line:
-                    linecontent=line.split(":")
-                    if linecontent[1].lstrip()!="-":
-                        role=linecontent[0]
-                        if "/" in linecontent[0]:
-                            filler_elements=linecontent[0].split("/")
-                            for i in range(0,len(filler_elements)):
-                                each_entry=filler_elements[i].lstrip().rstrip()
-                                create_pattern(each_entry, filedir, filename)
-                        else:
-                            each_entry=linecontent[1].lstrip().rstrip()
-                            create_pattern(each_entry, filedir, filename)
-                            
-                        prevrole=role
-  
-                else:
-                    content=line.lstrip().rstrip()
-                    if "/" in content:
-                        filler_elements=content.split("/")
-                        for i in range(0,len(filler_elements)):
-                            each_entry=filler_elements[i].lstrip().rstrip()
-                            create_pattern(each_entry, filedir, filename)
-                    else:
-                        each_entry=content.lstrip().rstrip()
-                        create_pattern(each_entry, filedir, filename)
-                        
-                    role=prevrole
+def main(argv):
+	readAndParseFile('developset/texts', 'developset/answers', "DEV")
                 
-                
+	print(argv)
 
-        #print filename
-        #print answer
+def readAndParseFile(dirnametxt, dirnameans, startsw):
+	filedir = dirnametxt
+	for filename in os.listdir(filedir):
+		print (filename)
+		print ("###############################################################")
         
+		answer=find(filename, dirnameans)
+		with open(answer+".anskey", 'r') as input_file:
+			prevrole=""
+			for line in input_file:
+				if ":" in line:
+					linecontent=line.split(":")
+					if linecontent[1].lstrip()!="-":
+						role=linecontent[0]
+						if "/" in linecontent[0]:
+							filler_elements=linecontent[0].split("/")
+							for i in range(0,len(filler_elements)):
+								each_entry=filler_elements[i].lstrip().rstrip()
+								create_pattern(each_entry, filedir, filename)
+						else:
+							each_entry=linecontent[1].lstrip().rstrip()
+							create_pattern(each_entry, filedir, filename)
+                            
+						prevrole=role
+  
+				else:
+					content=line.lstrip().rstrip()
+					if "/" in content:
+						filler_elements=content.split("/")
+						for i in range(0,len(filler_elements)):
+							each_entry=filler_elements[i].lstrip().rstrip()
+							create_pattern(each_entry, filedir, filename)
+					else:
+						each_entry=content.lstrip().rstrip()
+						create_pattern(each_entry, filedir, filename)
+                        
+					role=prevrole
+
         
 def create_pattern(each_entry, filedir, txt_file_name):
     with open(filedir+"/"+txt_file_name, 'r') as textfile:
@@ -56,20 +68,11 @@ def create_pattern(each_entry, filedir, txt_file_name):
             if each_entry in sents[i]:
                 print (sents[i])
                 #todo
-        
-    
-
-
-
 
 def find(name, path):
     for root, dirs, files in os.walk(path):
         if name+".anskey" in files:
             return os.path.join(root, name)
 
-
-
-
-
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
