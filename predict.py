fd0512506a27928741e20eab5f50d9cd76b7d673
HeavenlyBerserker <hxu.yunocreative@gmail.com>
2017-11-07 10:10:04 -0700

    Trying to get stuff right, also handling files and predictions

--- predict.py
+++ predict.py
@@ -45,7 +45,9 @@ def main(argv):
 	#[i][2][0] = raw answers
 	#[i][2][1] = parsed answers
 	#See printed output for more details
+
 	t1Files = processFiles('developset/texts', 'developset/answers', "TST1")
+	#t1Files = processFiles('developset/texts', 'developset/answers', "DEV")
 
 	patterns = readPats("output/patterns.txt")
 	triggers = readTrigs("output/triggers.txt")
@@ -56,9 +58,23 @@ def main(argv):
 	#printList(words,0)
 	#print(triggers)
 
-	predictions = predict(t1Files, patterns, triggers, words)
+	
+	
+	files = processFilesFinal('sample-textfile.txt','')
+	printFiles2(files)
+
+	#predictions = predict(files, patterns, triggers, words,1)
+	predictions = predict(t1Files, patterns, triggers, words,0)
+
+	writePred(predictions)
 
 	print(argv)
+
+#Write predict file
+def writePred(predictions):
+	file = open("output/predicitons.txt", 'w')
+	for line in predictions:
+		file.write(line)
 
 #Reads triggers from a file
 def readTrigs(name):
@@ -79,7 +95,7 @@ def readWords(name):
 		trigs.append(temp)
 	return trigs
 
-def predict(files, patterns, triggers, words):
+def predict(files, patterns, triggers, words, test):
 	tags = []
 	ans = []
 	sentences = []
@@ -87,7 +103,8 @@ def predict(files, patterns, triggers, words):
 
 	for i in range(len(files)):
 		tags.append(files[i][1][3])
-		ans.append(files[i][2][1])
+		if not test:
+			ans.append(files[i][2][1])
 		sentences.append(files[i][1][1])
 		raw.append(files[i][1][0])
 
@@ -110,10 +127,17 @@ def predict(files, patterns, triggers, words):
 		printList(ans[i], 0)
 		print("############################\n\n\n")
 	'''
-
+	count = 0
+	right = 0
 	for i in range(len(tags)):
+		count += 1
 		predict = []
 		inc = predInc(raw[i], words)
+		predict.append(['INCIDENT', inc])
+		if not test:
+			if inc == ans[i][1][1][0]:
+				right += 1
+
 		for j in range(len(tags[i])):
 			sentence = tags[i][j]
 			sent = sentences[i][j]
@@ -134,10 +158,111 @@ def predict(files, patterns, triggers, words):
 		predicts.append(predict)
 		print("############################\nPredictions:")
 		printList(predict, 0)
-		print("----------------------------\nActual Answers:")
-		printList(ans[i], 0)
+		if not test:
+			print("----------------------------\nActual Answers:")
+			printList(ans[i], 0)
 		print("############################\n\n\n")
-	return predicts
+		if not test:
+			print("---------------------\nAnswers included in predictors")
+			prediRightPred(predict, ans[i])
+			print("---------------------\nAnswers predictor got right")
+			prediRight(predict, ans[i])
+			print("---------------------")
+	if not test:
+		print("Right = " + str(right) + "/" + str(count))
+
+	predictions = []
+	for i in range(len(predicts)):
+		ent = predicts[i]
+		Id = files[i][0]
+		incident = []
+		weapon = []
+		indiv = []
+		org = []
+		target = []
+		victim = []
+		out = ''
+		for entry in ent:
+			if entry[0] == "INCIDENT":
+				incident.append(entry[1])
+			if entry[0] == "WEAPON":
+				weapon.append(entry[1])
+			if entry[0] == "PERP INDIV":
+				indiv.append(entry[1])
+			if entry[0] == "PERP ORG":
+				org.append(entry[1])
+			if entry[0] == "TARGET":
+				target.append(entry[1])
+			if entry[0] == "VICTIM":
+				victim.append(entry[1])
+		out += '\nID:             ' + Id + '\nINCIDENT:       '
+		if len(incident) > 0:
+			out += incident[0]
+		else:
+			out += '-'
+		for j in range(1,len(incident)):
+			out += "\n                " + indicent[j]
+		out += '\nWEAPON:         '
+		if len(weapon) > 0:
+			out += weapon[0]
+		else:
+			out += '-'
+		for j in range(1,len(weapon)):
+			out += "\n                " + weapon[j]
+		out += '\nPERP INDIV:     '
+		if len(indiv) > 0:
+			out += indiv[0]
+		else:
+			out += '-'
+		for j in range(1,len(indiv)):
+			out += "\n                " + indiv[j]
+		out += '\nPERP ORG:       '
+		if len(org) > 0:
+			out += org[0]
+		else:
+			out += '-'
+		for j in range(1,len(org)):
+			out += "\n                " + org[j]
+		out += '\nTARGET:         '
+		if len(target) > 0:
+			out += target[0]
+		else:
+			out += '-'
+		for j in range(1,len(target)):
+			out += "\n                " + target[j]
+		out += '\nVICTIM:         '
+		if len(victim) > 0:
+			out += victim[0]
+		else:
+			out += '-'
+		for j in range(1,len(victim)):
+			out += "\n                " + victim[j]
+
+		predictions.append(out)
+		#print(out)
+
+	return predictions
+
+def prediRight(predict, answer):
+	for item in answer:
+		typ = item[0]
+		for i in range(1, len(item)):
+			ansor = item[i]
+			for ans in ansor:
+				for pred in predict:
+					if ans == pred[1] and typ == pred[0]:
+						print(pred)
+
+def prediRightPred(predict, answer):
+	for item in answer:
+		typ = item[0]
+		for i in range(1, len(item)):
+			ansor = item[i]
+			for ans in ansor:
+				for pred in predict:
+					if ans == pred[1] or ans in pred[1] or pred[1] in ans:
+						print(pred)
+
 
 def readPats(filename):
 	file = open(filename, 'r')
@@ -173,6 +298,213 @@ def predInc(text, words):
 			maxval = incs[key]
 			maxlab = key
 	print(maxlab)
+	return maxlab
+
+#Function#########################################################
+#Processes the test in various ways to then store them back into the array
+#It does it without losing anything
+def processFilesFinal(filein, filean):
+	#Reads and parses the files into the following format
+	#files = [file_index 0-i] 
+	#		 [0 = filename, 1 = text_file, 2 = answers_file] 
+	#		 [0 = plain_text]
+	files = readAndParseFileFinal(filein, filean)
+
+	#Extracts sentences in files[i][1][1]
+	files = extractSentences2(files)
+
+	return files
+
+def readAndParseFileFinal(filein, filean):
+	txtFiles = []
+	ansFiles = []
+	files = []
+	
+	
+			#print ("###############################################################")
+			#print (filename)
+
+	#print(answer)
+
+	tfile = open(filein,"r")
+
+	rawText = tfile.read()
+
+	words = nltk.word_tokenize(rawText)
+
+	rawText = []
+	good = ''
+	for word in words:
+		if (word.startswith("DEV-MUC") or word.startswith("TST1-MUC") or word.startswith("TST2-MUC")) and len(good) > 1:
+			rawText.append([good[good.index("--"):],word])
+			good = ''
+		good += ' ' + word
+	#print(rawText)
+
+	for f in rawText:
+		text = [ f[1] ,[f[0]]]
+
+		files.append(text)
+
+	#print(files)
+	return files
+	'''
+	with open(answer, 'r') as input_file:
+		prevrole=""
+		for line in input_file:
+			if ":" in line:
+				linecontent=line.split(":")
+				if linecontent[1].lstrip()!="-":
+					role=linecontent[0]
+					if "/" in linecontent[0]:
+						filler_elements=linecontent[0].split("/")
+						for i in range(0,len(filler_elements)):
+							each_entry=filler_elements[i].lstrip().rstrip()
+							create_pattern(each_entry, filedir, filename)
+					else:
+						each_entry=linecontent[1].lstrip().rstrip()
+						create_pattern(each_entry, filedir, filename)
+					prevrole=role
+			else:
+				content=line.lstrip().rstrip()
+				if "/" in content:
+					filler_elements=content.split("/")
+					for i in range(0,len(filler_elements)):
+						each_entry=filler_elements[i].lstrip().rstrip()
+						create_pattern(each_entry, filedir, filename)
+				else:
+					each_entry=content.lstrip().rstrip()
+					create_pattern(each_entry, filedir, filename)
+				role=prevrole
+	'''
+
+def extractSentences2(files):
+	nlp = spacy.load('en')
+	for i in range(len(files)):
+		#print("Processing " + str(i) +"/" + str(len(files)) + " files")
+		file = files[i][1][0]
+		sentences = nltk.sent_tokenize(file)[0:]
+		sentenceEntities = []
+		sentenceImp = []
+		for j in range(len(sentences)):
+			sentences[j] = re.sub('\n', ' ', sentences[j])
+			#tokens = nltk.word_tokenize(sentences[j])
+			#tagged = nltk.pos_tag(tokens)
+			doc=nlp(unicode(sentences[j].lower()))
+			
+			chunks = []
+			for np in doc.noun_chunks:
+				chunk = []
+				chunk.append(unicodedata.normalize('NFKD', np.text.upper()).encode('ascii','ignore'))
+				chunk.append(unicodedata.normalize('NFKD', np.root.text.upper()).encode('ascii','ignore'))
+				chunk.append(unicodedata.normalize('NFKD', np.root.dep_).encode('ascii','ignore'))
+				chunk.append(unicodedata.normalize('NFKD', np.root.head.text.upper()).encode('ascii','ignore'))
+				chunks.append(chunk)
+
+
+			##################Test lab##########################
+			#print('\n' + sentences[j])
+			tokens = nltk.word_tokenize(sentences[j].lower())
+			tagged = nltk.pos_tag(tokens)
+			#print(tagged)
+			tags = []
+			for k in range(len(tagged)):
+				tags.append([tagged[k][0].upper(),tagged[k][1]])
+			#print(tags)
+			verbsAndPos = []
+			k=0
+			while k < len(tags):
+				t = tags[k]
+				s = k
+				vs = []
+				if t[1][0:2] == 'VB' and t[0].isalpha():
+					vs.append(t[0])
+					k+=1
+					while k < len(tags) and tags[k][1][0:2] == 'VB' and tags[k][0].isalpha():
+						vs[0] = vs[0] + " " + tags[k][0]
+						k+=1
+					while k < len(tags) and tags[k][0] != ',' and tags[k][0] != ':' and tags[k][0] != ';':
+						k+=1
+					vs.append(k)
+					vs.append(s)
+				if len(vs) > 0 and len(vs[0]) > 0:
+					verbsAndPos.append(vs)
+				k+=1
+			if len(verbsAndPos) > 0:
+				verbsAndPos[len(verbsAndPos)-1][1] = len(tags)-1
+
+			chunks2 = []
+			k=0
+			while k < len(tags):
+				t = tags[k]
+				s = k
+				vs = []
+				if (t[1][0:2] == 'IN') and t[0].isalpha():
+					prep = t[0]
+					vs.append("")
+					vs.append("")
+					k+=1
+					while k < len(tags) and (tags[k][1][0:2] == 'NN' or tags[k][1][0:2] == 'JJ'  or tags[k][1][0:2] == 'JJ')and tags[k][0].isalpha():
+						vs[0] = vs[0] + " " + tags[k][0]
+						k+=1
+					vs.append(prep.lower())
+					vs.append(retVerb(k, verbsAndPos))
+				elif (t[1][0:2] == 'NN' or t[1][0:2] == 'DT') and t[0].isalpha():
+					vs.append(t[0])
+					vs.append(t[0])
+					k+=1
+					while k < len(tags) and (tags[k][1][0:2] == 'NN' or tags[k][1][0:2] == 'JJ'  or tags[k][1][0:2] == 'JJ')and tags[k][0].isalpha():
+						vs[0] = vs[0] + " " + tags[k][0]
+						k+=1
+					vs.append(subobj(k, verbsAndPos))
+					vs.append(retVerb(k, verbsAndPos))
+				if len(vs) > 0:
+					chunks2.append(vs)
+				if k == s:
+					k+=1
+			#print(verbsAndPos)
+			#printList(chunks,0)
+			#print("Chunks2")
+			#printList(chunks2,0)
+
+			#[to_nltk_tree(sent.root).pretty_print() for sent in doc.sents]
+			#####################################################
+			#sentenceEntities.append(sub_toks)
+			'''
+			entities = nltk.chunk.ne_chunk(tagged)
+			sentenceEntities.append(entities)'''
+			sentenceEntities.append(chunks2)
+			'''
+			impWords = []
+			for important in files[i][2][1]:
+				for k in range(1, len(important)):
+					for l in range(len(important[k])):
+						if not important[k][l].startswith("DEV") and important[k][l] != '-' and important[k][l] in sentences[j]:
+							impWords.append([important[k][l], find_indexes(important[k][l], sentences[j]), important[0]])
+			sentenceImp.append(impWords)
+			'''
+			
+
+		files[i][1].append(sentences)
+		#files[i][1].append(sentenceEntities)
+		files[i][1].append(sentenceImp)
+		files[i][1].append(sentenceEntities)
+	return files
+
+#Prints files content
+def printFiles2(files):
+	for i in range(len(files)):
+		print("\n\n\n")
+		print("#################Filename####################")
+		print(files[i][0]) #prints name of file i
+		print("#################Raw_Text####################")
+		print(files[i][1][0]) #prints text for file i
+		print("Parsed sentences-----------------------------")
+		printList(files[i][1][1],2)
+		print("Important words------------------------------")
+		printList(files[i][1][2],2)
+		print("Tagger output------------------------------")
+		printList(files[i][1][3],2)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
