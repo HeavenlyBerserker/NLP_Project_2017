dc33a2d7ef197210e50bb03125c69f984080379e
HeavenlyBerserker <hxu.yunocreative@gmail.com>
2017-11-07 19:34:02 -0700

    CADEing

--- infoextract.sh
+++ infoextract.sh
@@ -1,6 +1,7 @@
 pip install --user virtualenv
 mkdir venv
-python -m virtualenv venv 
+python -m virtualenv venv
+source venv/bin/activate 
 pip install --user spacy
 python -m spacy.en.download all
 pip install -user nltk
